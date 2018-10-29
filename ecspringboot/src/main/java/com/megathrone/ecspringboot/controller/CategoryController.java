c45469a6aa73535474ba7328c6be533eea4fa895
Megathrone <yizhouwang418@gmail.com>
2018-10-29 16:18:06 +0800

    page function done.

--- ecspringboot/src/main/java/com/megathrone/ecspringboot/controller/CategoryController.java
+++ ecspringboot/src/main/java/com/megathrone/ecspringboot/controller/CategoryController.java
@@ -2,9 +2,10 @@ package com.megathrone.ecspringboot.controller;
 
 import com.megathrone.ecspringboot.bean.Category;
 import com.megathrone.ecspringboot.service.CategoryService;
-import java.util.List;
+import com.megathrone.ecspringboot.util.PageForNavigator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,7 +13,12 @@ public class CategoryController {
   @Autowired CategoryService categoryService;
 
   @GetMapping("/categories")
-  public List<Category> list() {
-    return categoryService.list();
+  public PageForNavigator<Category> list(
+      @RequestParam(value = "start", defaultValue = "0") int start,
+      @RequestParam(value = "size", defaultValue = "5") int size)
+      throws Exception {
+    start = start < 0 ? 0 : start;
+    PageForNavigator<Category> page = categoryService.list(start, size, 5);
+    return page;
   }
 }
--- ecspringboot/src/main/java/com/megathrone/ecspringboot/service/CategoryService.java
+++ ecspringboot/src/main/java/com/megathrone/ecspringboot/service/CategoryService.java
@@ -2,8 +2,12 @@ package com.megathrone.ecspringboot.service;
 
 import com.megathrone.ecspringboot.bean.Category;
 import com.megathrone.ecspringboot.dao.CategoryDAO;
+import com.megathrone.ecspringboot.util.PageForNavigator;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
@@ -12,8 +16,15 @@ import org.springframework.stereotype.Service;
 public class CategoryService {
   @Autowired CategoryDAO categoryDAO;
 
+  public PageForNavigator<Category> list(int start, int size, int navigatePages) {
+    Sort sort = new Sort(Sort.Direction.DESC, "id");
+    Pageable pageable = new PageRequest(start, size,sort);
+    Page pageFromJPA =categoryDAO.findAll(pageable);
+
+    return new PageForNavigator<>(pageFromJPA,navigatePages);
+  }
   public List<Category> list() {
-    Sort sort = new Sort(Direction.DESC, "id");
+    Sort sort = new Sort(Sort.Direction.DESC, "id");
     return categoryDAO.findAll(sort);
   }
 }
