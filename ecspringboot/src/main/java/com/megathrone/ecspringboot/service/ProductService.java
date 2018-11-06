8b9826d795bea384ce6b3ac5bfee957fde571651
joelw <yizhouwang418@gmail.com>
2018-11-06 11:38:12 +0800

    product admin (#8)

---
+++ ecspringboot/src/main/java/com/megathrone/ecspringboot/service/ProductService.java
@@ -0,0 +1,42 @@
+package com.megathrone.ecspringboot.service;
+
+import com.megathrone.ecspringboot.bean.Category;
+import com.megathrone.ecspringboot.bean.Product;
+import com.megathrone.ecspringboot.dao.ProductDAO;
+import com.megathrone.ecspringboot.util.PageForNavigator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+  @Autowired ProductDAO productDAO;
+  @Autowired CategoryService categoryService;
+
+  public void add(Product product) {
+    productDAO.save(product);
+  }
+
+  public void delete(int id) {
+    productDAO.deleteById(id);
+  }
+
+  public Product get(int id) {
+    return productDAO.findById(id).get();
+  }
+
+  public void update(Product product) {
+    productDAO.save(product);
+  }
+
+  public PageForNavigator<Product> list(int cid, int start, int size, int navigatePages) {
+    Category category = categoryService.get(cid);
+    Sort sort = new Sort(Sort.Direction.DESC, "id");
+    Pageable pageable = new PageRequest(start, size, sort);
+    Page<Product> pageFromJPA = productDAO.findByCategory(category, pageable);
+    return new PageForNavigator<>(pageFromJPA, navigatePages);
+  }
+}
