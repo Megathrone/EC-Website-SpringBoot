2acfc96b647a065d15ac226cedcb62b3de2bc033
Megathrone <yizhouwang418@gmail.com>
2018-11-08 16:25:24 +0800

    User admin

---
+++ ecspringboot/src/main/java/com/megathrone/ecspringboot/dao/UserDAO.java
@@ -0,0 +1,8 @@
+package com.megathrone.ecspringboot.dao;
+
+import com.megathrone.ecspringboot.bean.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserDAO extends JpaRepository<User, Integer> {
+
+}
