686c9ba584f97bd1b8f181cc16ca8fb55dda7c59
Megathrone <yizhouwang418@gmail.com>
2018-11-14 15:20:01 +0800

    other interceptor

--- ecspringboot/src/main/java/com/megathrone/ecspringboot/config/WebMvcConfigurer.java
+++ ecspringboot/src/main/java/com/megathrone/ecspringboot/config/WebMvcConfigurer.java
@@ -1,6 +1,7 @@
 package com.megathrone.ecspringboot.config;
 
 import com.megathrone.ecspringboot.interceptor.LoginInterceptor;
+import com.megathrone.ecspringboot.interceptor.OtherInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -9,6 +10,11 @@ public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
   @Bean
   public LoginInterceptor getLoginIntercepter() {
     return new LoginInterceptor();
+  }
+
+  @Bean
+  public OtherInterceptor getOtherIntercepter() {
+    return new OtherInterceptor();
   }
 
   @Override
